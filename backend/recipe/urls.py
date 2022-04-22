c710e2f5c3ef006e6eb2035e886551cdbfc1cfd7
Creepy-Panda <oxi3der@gmail.com>
2022-04-22 12:10:12 +1000

    del unuse import

--- backend/recipe/urls.py
+++ backend/recipe/urls.py
@@ -1,7 +1,5 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from django.conf.urls.static import  static
-from django.conf import settings
 
 from .views import RecipeViewSet, TagsViewSet, IngredientsViewSet
 
